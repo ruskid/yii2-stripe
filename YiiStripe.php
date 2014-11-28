556ad9b4fa53c74586daad26baad1fc69a58699b
Victor Demin <demmbox@gmail.com>
2014-11-28 15:11:04 +0100

    Create YiiStripe.php

---
+++ YiiStripe.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @copyright Copyright Victor Demin, 2014
+ * @license https://github.com/ruskid/yii-stripe/LICENSE
+ * @link https://github.com/ruskid/yii-stripe#readme
+ */
+
+namespace ruskid\stripe;
+
+use yii\base\Exception;
+
+/**
+ * Yii stripe component.
+ *
+ * @author Victor Demin <demmbox@gmail.com>
+ */
+class YiiStripe extends \yii\base\Component {
+
+    /**
+     * @see Stripe
+     * @var string Stripe's public key
+     */
+    public $publicKey;
+
+    /**
+     * @see Stripe
+     * @var string Stripe's private key
+     */
+    public $privateKey;
+
+    /**
+     * @see Init extension default
+     */
+    public function init() {
+        if (!$this->publicKey) {
+            throw new Exception("Stripe's public key is not set.");
+        } elseif (!$this->privateKey) {
+            throw new Exception("Stripe's private key is not set.");
+        }
+        parent::init();
+    }
+
+}
