b77f270bf812b3aefe9502f965fa2baa08d06c6a
Victor Demin <demmbox@gmail.com>
2014-12-09 16:56:32 +0100

    Update and rename YiiStripeHelper.php to StripeHelper.php

--- StripeHelper.php
+++ StripeHelper.php
@@ -8,12 +8,14 @@
 
 namespace ruskid\stripe;
 
+use yii\base\Exception;
+
 /**
  * Yii Stripe helper class.
  *
  * @author Victor Demin <demmbox@gmail.com>
  */
-class YiiStripeHelper {
+class StripeHelper {
 
     /**
      * If the value is boolean, then it must be in quotes.
@@ -25,4 +27,16 @@ class YiiStripeHelper {
         }
     }
 
+    /**
+     * Check Stripe's documentation. You should not use name tag for card inputs.
+     * @param array $options
+     * @throws Exception
+     */
+    public static function secCheck($options) {
+        if (isset($options['name'])) {
+            throw new Exception("Do not use 'name' tag for number/cvc/month/year inputs.");
+        }
+    }
+
 }
+
